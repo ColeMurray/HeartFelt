438d9c1bfa9ea31d9712ee58001090794372d0c1
Cole Murray <colemurray.cs@gmail.com>
2015-05-23 16:08:59 -0700

    FIX logout now redirects to home

--- FirstProject/nodetest1/public/js/controllers/navctrl.js
+++ FirstProject/nodetest1/public/js/controllers/navctrl.js
@@ -1,8 +1,9 @@
 angular.module('NavCtrl',['AuthService'])
-	.controller('NavController', function($scope,Auth){
+	.controller('NavController', function($scope,Auth, $state){
 		$scope.loggedIn = Auth.isLoggedIn;	
 
 		$scope.logout = function(){
 			Auth.logout();
+			$state.go('home');
 		};
 	})
